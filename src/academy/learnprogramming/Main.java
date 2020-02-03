aaef2b0d3a2644c5861b1663426be3d042c3af00
kevinnoon <kevin.noon@ntlworld.com>
2020-02-03 09:07:00 +0000

    initial changes

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi GitHub");
+	// write your code here
+    }
+}
